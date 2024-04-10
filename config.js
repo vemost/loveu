b99fbcadec094c0c56d4d62bcfe6fd544c98d489
vemost <66854381+vemost@users.noreply.github.com>
2024-04-10 18:14:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "chỉ muốn ngắm em cả đêm",
     introTitle: 'hold lấy em từ đằng sau gáy, damn',
-    introDesc: `Mong em sẽ không nhớ về
-    uh
-    Một người từng thương em nhiều như thế
-    yeah người đã thương em nhiều tư thế`,
-    btnIntro: 'chỉ muốn ngắm em cả đêm',
-    title: 'Đi nhà nghỉ với anh hong 🥰',
+    introDesc: `Nhà nghỉ hay nhà khách là nơi người ta làm dịch vụ nghỉ ngơi cho khách qua đường. Người ta có thể thuê phòng ở đây để nghỉ. Một nhà nghỉ có nhiều phòng, ...`,
+    btnIntro: 'Hiểu rồi thì nhấn vô đây',
+    title: 'Đi nhà nghỉ với anh hong 🙂',
     desc: 'Bò, lăn, trườn, vuốt ve cặp chân giò măng chườm thuốc mê Còn hai quả banh chuyền cúp D',
     btnYes: 'Đi chứ <33',
     btnNo: 'Nghỉ cái dmm :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 8 tỉ người mà sao bạn lại đi nhà nghỉ với tôi',
+    btnReply: 'nhấn vô đi mày <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Tao biết mà 🙂',
+    messDesc: 'Tối nay 7h, qua đón đi nghỉ nha.',
+    btnAccept: '🙂',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
